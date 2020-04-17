750a5fded0a2d3f794dad8a71ceb8dc970c84924
Batista Harahap <batista@bango29.com>
2020-04-17 15:53:36 +0700

    playbook base class

--- neobabix/playbooks/playbook.py
+++ neobabix/playbooks/playbook.py
@@ -1,6 +1,29 @@
 from ccxt.base.exchange import Exchange
+from asyncio import Lock
+from logging import Logger
 
 
 class Playbook(object):
-    def __init__(self, exchange: Exchange):
+    __name__ = 'Neobabix Playbook'
+
+    def __init__(self, exchange: Exchange, trade_lock: Lock, logger: Logger):
         self.exchange = exchange
+        self.trade_lock = trade_lock
+        self.logger = logger
+
+        # Acquire lock immediately
+        self.trade_lock.acquire()
+
+    def info(self, message):
+        self.logger.info(f'{self.__name__}: {message}')
+
+    def debug(self, message):
+        self.logger.debug(f'{self.__name__}: {message}')
+
+    def release_trade_lock(self):
+        self.trade_lock.release()
+
+    def __del__(self):
+        # Explicit lock release
+        if self.trade_lock.locked():
+            self.release_trade_lock()
